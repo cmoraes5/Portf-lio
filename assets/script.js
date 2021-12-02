8704c146cda660a9cfa821a0448927f7240aade2
cmoraes5 <moraescaio0502@gmail.com>
2021-12-02 16:26:09 -0300

    :up: Atualizações, :package:

--- assets/script.js
+++
@@ -1,6 +0,0 @@
-//              Menu Sidebar
-var hamburguer = document.querySelector(".hamburguer")
-
-hamburguer,addEventListener("click", function(){
-    document.querySelector(".container").classList.toggle("show-menu")
-});
